c4e58af1c7b472ee1b1d60d3f36c0ae31761237e
Mathieu David <mathieudavid@mathieudavid.org>
2024-04-22 16:41:05 +0200

    Use zgenom to manage zsh plugins and fix issues
    Use zgenom to manage zsh plugins. Add fzf-tab
    plugin to have better tab completion.
    
    Fix alacritty zellij session script and add it
    to be managed by chezmoi.

---
+++ home/dot_config/zellij/executable_zellij-new-or-attach.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ZELLIJ_SESSIONS=$({ echo "NEW SESSION"; zellij ls; })
+CHOSEN_SESSION=$(echo "$ZELLIJ_SESSIONS" | fzf -0 -1 --ansi --reverse | sed 's/^\(.*\) \[Created.*$/\1/')
+
+if [[ $CHOSEN_SESSION == "NEW SESSION" ]]; then
+    zellij
+else
+    zellij attach "$CHOSEN_SESSION"
+fi
