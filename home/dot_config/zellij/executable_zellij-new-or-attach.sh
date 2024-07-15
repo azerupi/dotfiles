e9927f7b92a5581800fac4b668b3525e09762c8a
Mathieu David <mathieudavid@mathieudavid.org>
2024-07-15 19:27:05 +0200

    Fix zshrc not loading before alacritty on Ubuntu 24.04

--- home/dot_config/zellij/executable_zellij-new-or-attach.sh
+++ home/dot_config/zellij/executable_zellij-new-or-attach.sh
@@ -1,13 +1,23 @@
 #!/bin/zsh
 
-ZELLIJ_SESSIONS=$({
-    echo "NEW SESSION"
-    zellij ls
-})
-CHOSEN_SESSION=$(echo "$ZELLIJ_SESSIONS" | fzf -0 -1 --ansi --reverse | sed 's/^\(.*\) \[Created.*$/\1/')
-
-if [[ $CHOSEN_SESSION == "NEW SESSION" ]]; then
-    zellij
+if ! command -v zellij &>/dev/null; then
+    echo "zellij not found"
+    echo "$PATH"
+    sleep 1
+elif ! command -v fzf &>/dev/null; then
+    echo "fzf not found"
+    echo "$PATH"
+    sleep 1
 else
-    zellij attach "$CHOSEN_SESSION"
+    ZELLIJ_SESSIONS=$({
+        echo "NEW SESSION"
+        zellij ls
+    })
+    CHOSEN_SESSION=$(echo "$ZELLIJ_SESSIONS" | fzf -0 -1 --ansi --reverse | sed 's/^\(.*\) \[Created.*$/\1/')
+
+    if [[ $CHOSEN_SESSION == "NEW SESSION" ]]; then
+        zellij
+    else
+        zellij attach "$CHOSEN_SESSION"
+    fi
 fi
